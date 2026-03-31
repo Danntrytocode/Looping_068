ed0bf6627107527a085e3fc35ead08fa6a13c901
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-31 18:42:02 +0700

    menambah kan main program

--- logikaand.cpp
+++ logikaand.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan Nilai Bahasa Iggris =";
+    cin>> nilB;
+    cout<<"Masukkan Nilai Matematika =";
+    cin>> nilM;
+
+    rerata = (nilB+nilM)/2;
+
+
+}
