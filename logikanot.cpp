19ebdddb95ed979e63a0636fb595707a10df27ab
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-31 17:50:18 +0700

    fungsi dan if else

--- logikanot.cpp
+++ logikanot.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main(){
-    float nilB,nilM,rerata;
+    float nilM,nilB,rerata;
     string status;
+
+    cout<<"Masukan Nilai Matematika =" ;
+    cin>>nilM;
+    cout<<"Masukkan Nilai Bahasa Inggris =" ;
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if (!(rerata < 60) )
+        status = "LULUS";
+    else
+        status = "TIDAK LULUS";
     
+    cout<<"Status Kelulusan ="<<status<<" ,dengan nilai rata-rata ="<<rerata<< endl;
 }
