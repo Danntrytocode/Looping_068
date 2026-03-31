5cee81edf5dfbdf959a0d3859bf1e72f5d1f57ae
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-31 18:36:08 +0700

    prorgram utama, fungsi dan if else

--- logikaor.cpp
+++ logikaor.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    float nilM,nilB,rerata;
+    string status;
+    
+    cout<<"Masukkan nilai Matematika =";
+    cin>> nilM;
+    cout<<"Masukkan nilai Bahasa Inggris =";
+    cin>> nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if(!(rerata >= 60) || (nilM >= 70))
+        status = "LULUS";
+    else
+        status = "TIDAK LULUS";
+    
+    cout<<"Status Kelulusan ="<<status<<" ,dengan nilai rata-rata ="<<rerata<< endl;
+}
