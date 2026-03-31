49970e1a01175f7a23b696075f0057553b77a8bc
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-03-31 18:49:46 +0700

    menambahkan deklarasi dan prosedur input

--- switchCase.cpp
+++ switchCase.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+//Deklarasi variabel global
+int KodeJurusan;
+string namaJurusan;
+string statusKuota;
+
+//Prosedur Input
+void input( ) {
+    cout<< "=== PENDAFTARAN JURUSAN KAMPUS ==="<< endl;
+    cout<< "1. Teknologi Informasi"<< endl;
+    cout<< "2. Teknik Mesin"<< endl;
+    cout<< "3. Sistem Informasi"<< endl;
+    cout<< "4. Ilmu Komunikasi"<< endl;
+    cout<< "-----------------------------------"<< endl;
+    cout<< "Masukkan kode Jurusan (1-4): ";
+    cin>> KodeJurusan;
+}
